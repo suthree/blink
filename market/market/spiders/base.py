7020f0fe2ce7c53d1950d974482816333d093a42
alex_song <alex_song@wochacha.com>
2020-08-19 10:03:31 +0800

    update

--- market/market/spiders/base.py
+++ market/market/spiders/base.py
@@ -2,4 +2,9 @@ from scrapy_redis.spiders import RedisSpider
 
 
 class BaseSpider(RedisSpider):
-    pass
+
+    def check_task(self):
+        """
+        查询库中有多少待爬门店及条码数据
+        """
+        pass
